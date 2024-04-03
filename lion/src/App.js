ede4bef7ad6917102e61c11334541464f3ead343
heeyoung123 <heeyoung321@naver.com>
2024-04-03 16:50:47 +0900

    #1 -

--- lion/src/App.js
+++ lion/src/App.js
@@ -1,13 +1,8 @@
 import "./App.css";
 import PropsPractice from "./Ex";
 
-/*function App() {
-  let data = { id: 4, pw: 78 };
-  return <Ex props={data}></Ex>;
-}*/
-
 function App() {
-  let data = { first: 4, second: 8 };
+  let data = { id: 4, pw: 8 };
   return <PropsPractice props={data}></PropsPractice>;
 }
 
--- lion/src/Ex.jsx
+++ lion/src/Ex.jsx
@@ -1,18 +1,10 @@
 import "./App.css";
 
-/*function Ex({ props }) {
+function PropsPractice({ props }) {
   return (
     <div>
       {props.id}
       {props.pw}
-    </div>
-  );
-}*/
-function PropsPractice({ props }) {
-  return (
-    <div>
-      {props.first}
-      {props.second}
     </div>
   );
 }
